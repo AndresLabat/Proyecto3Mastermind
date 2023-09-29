e85df055f6c8a7697e8a0b5f4b25b9180a5161d5
AndrésLabat <andreslabat89@gmail.com>
2023-09-29 16:31:24 +0200

    feat: added new color selectors based on the chosen difficulty

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@ const color1 = document.getElementById("color1");
 const color2 = document.getElementById("color2");
 const color3 = document.getElementById("color3");
 const color4 = document.getElementById("color4");
+const color5 = document.getElementById("color5");
+const color6 = document.getElementById("color6");
 
 const muestraColor1 = document.getElementById("muestraColor1");
 const muestraColor2 = document.getElementById("muestraColor2");
 const muestraColor3 = document.getElementById("muestraColor3");
 const muestraColor4 = document.getElementById("muestraColor4");
+const muestraColor5 = document.getElementById("muestraColor5");
+const muestraColor6 = document.getElementById("muestraColor6");
 
 const nombreJugador = document.getElementById("nombreJugador");
 
@@ -29,6 +33,16 @@ color3.addEventListener("input", ()=>{
 color4.addEventListener("input", ()=>{
     muestraColor4.style.backgroundColor = color4.value;
     localStorage.setItem("color4", color4.value);
+})
+
+color5.addEventListener("input", ()=>{
+    muestraColor5.style.backgroundColor = color5.value;
+    localStorage.setItem("color5", color5.value);
+})
+
+color6.addEventListener("input", ()=>{
+    muestraColor6.style.backgroundColor = color6.value;
+    localStorage.setItem("color6", color6.value);
 })
 
 nombreJugador.addEventListener("input", ()=>{
